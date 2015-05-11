250cc46c1b20a7c3d3a4ba09768293cca8cf5e4b
urandu <bildadnamawa@gmail.com>
2015-05-11 22:47:59 +0300

    web API complete

--- application/controllers/stops.php
+++ application/controllers/stops.php
@@ -7,9 +7,26 @@ class Stops extends CI_Controller {
 	{
 		$this->load->view('welcome_message');
 	}
-
-    public function get_trip($origin_lat,$origin_lon,$destination_lat,$destination_lon)
+/*
+ * get_trip() will be open for API calls\
+ * it will return a json object comprising
+ *
+ *
+ *
+ * */
+    public function get_trip($origin_lat=null,$origin_lon=null,$destination_lat=null,$destination_lon=null)
     {
+        $intersection_stop_id=null;
+        $origin_route_town_terminal=null;
+        $destination_route_town_terminal=null;
+        $origin=null;
+        $destination=null;
+        $destination_route=null;
+        $origin_route=null;
+        $origin_and_destination_are_on_same_route=null;
+        $advice=null;
+        $final_route_advice=null;
+        $intersection=null;
         if(!empty($origin_lat) && !empty($origin_lon) && !empty($destination_lat) && !empty($destination_lon))//if the received request has data
         {
             $this->load->model('stop_model');
@@ -31,9 +48,11 @@ class Stops extends CI_Controller {
                         $origin_route = $origin_and_destination_are_on_same_route[0];
                         $advice = advice($origin, $destination, $origin_route);
                         if ($advice) {
-                            echo($advice);
+                     //       echo($advice);
+                            $final_route_advice=$advice;
                         } else {
-                            echo(-1);
+                       //     echo(-1);
+                            $final_route_advice=null;
                         }
                     } else //if stops are not on the same route
                     {
@@ -47,20 +66,22 @@ class Stops extends CI_Controller {
 
                         if ($intersection) //intersection exists
                         {
-                    echo(" intersection is there1");
+                   /* echo(" intersection is there1");
                     echo("<br>origin stop: ".$origin);
                     echo("<br>destination stop: ".$destination);
                     echo("<br>origin route: ".$origin_route);
                     echo("<br>destination route: ".$destination_route);
                     echo("<br>intersection: ");
-                    print_r(reset($intersection));
+                    print_r(reset($intersection));*/
                             $intersection_stop_id = reset($intersection); //get the first intersection point but need to include distance filters
                             $advice1 = advice($origin, $intersection_stop_id, $origin_route);
                             $advice2 = advice($intersection_stop_id, $destination, $destination_route);
-                            echo($advice1 . " " . $advice2);
+                            //echo($advice1 . " " . $advice2);
+                            $final_route_advice=$advice1 . " " . $advice2;
+                            $status=-1;
                         } else //no intersection exists
                         {
-                            echo("<br>no intersection exists");
+                            //echo("<br>no intersection exists");
 
                             $origin;
                             $destination;
@@ -72,23 +93,29 @@ class Stops extends CI_Controller {
                             $destination_route_town_terminal = get_route_town_terminus($destination_route);
 
 
-                            echo("<br>origin stop: ".$origin);
+
+                            /*echo("<br>origin stop: ".$origin);
                              echo("<br>destination stop: ".$destination);
                              echo("<br>origin route: ".$origin_route);
                              echo("<br>destination route: ".$destination_route);
                              echo("<br>origin route terminal : ".$origin_route_town_terminal);
-                             echo("<br>destination route terminal : ".$destination_route_town_terminal."<br>");
+                             echo("<br>destination route terminal : ".$destination_route_town_terminal."<br>");*/
 
 
                             if ($origin_route_town_terminal && $destination_route_town_terminal) //successfully retrieved terminus for origin and destination
                             {
+                                //correction needed to add functionality of third route inclusion here....first check if origin route terminal is in cbd
                                 $advice1 = advice($origin, $origin_route_town_terminal, $origin_route);
                                 $advice2 = advice($destination_route_town_terminal, $destination, $destination_route);
-                                echo($advice1 . " " . $advice2);
+                               // echo($advice1 . " " . $advice2);
+                                $final_route_advice=$advice1 . " " . $advice2;
+                                $status=1;
                             } else //unsuccessfully retrieved terminus for origin and destination
                             {
 
-                                echo("error in retrieving route terminus");
+                                $final_route_advice=null;
+                                $status=-1;
+                                //echo("error in retrieving route terminus");
                             }
 
                         }
@@ -96,17 +123,60 @@ class Stops extends CI_Controller {
                     }
                 } else {
                     //return no result
-                    echo("There was an error: with the input ensure the destination or the origin is correct");
+                    //echo("There was an error: with the input ensure the destination or the origin is correct");
+                    $final_route_advice=null;
+                    $status=-1;
                 }
             } else //destination and origin are not set
             {
 
+                $final_route_advice=null;
+                $status=-1;
             }
         }
         else//if the parameters passed contain errors
         {
 
+            $final_route_advice=null;
+            $status=-1;
         }
+
+        //echo("<br>ufufufufufufu</br>");
+
+
+        $this->load->model('stop_model');
+        $origin_coordinates=array(
+            'lat'=>$origin_lat,
+            'lon'=>$origin_lon
+        );
+        $origin_stop_coordinates=$this->stop_model->get_stop_coordinates($origin);
+        //print_r($origin_stop_coordinates);
+        $origin_route_town_terminal_coordinates=$this->stop_model->get_stop_coordinates($origin_route_town_terminal);
+        $destination_route_town_terminal_coordinates=$this->stop_model->get_stop_coordinates($destination_route_town_terminal);
+        $destination_stop_coordinates=$this->stop_model->get_stop_coordinates($destination);
+        $destination_coordinates=array(
+            'lat'=>$destination_lat,
+            'lon'=>$destination_lon
+        );
+        $intersection_coordinates=$this->stop_model->get_stop_coordinates($intersection_stop_id);
+
+        $final_array=array(
+            'status'=>$status,
+            'advice'=>$final_route_advice,
+            'origin'=>$origin_coordinates,
+            'origin_stop'=>$origin_stop_coordinates,
+            'origin_terminal'=>$origin_route_town_terminal_coordinates,
+            'destination_terminal'=>$destination_route_town_terminal_coordinates,
+            'destination_stop'=>$destination_stop_coordinates,
+            'destination'=>$destination_coordinates,
+            'intersection'=>$intersection_coordinates
+        );
+
+       // print_r($final_array);
+        header('Content-Type: application/json');
+        echo(json_encode($final_array,JSON_PRETTY_PRINT));
+
+
     }
 
 
--- application/models/stop_model.php
+++ application/models/stop_model.php
@@ -8,6 +8,32 @@
 class Stop_model extends CI_Model
 {
 
+    public function get_stop_coordinates($stop_id="ooo")
+    {
+        $this->db->where('stop_id',$stop_id);
+        $this->db->limit(1);
+        $result=$this->db->get('stops');
+        //print_r( $result);
+        if($result->num_rows() > 0)
+        {
+
+            //echo('stop  coordinates details returned result');
+            //return $result->result()[0];
+            $result_array=array(
+                'lat'=>$result->result()[0]->stop_lat,
+                'lon'=>$result->result()[0]->stop_lon
+            );
+            return $result_array;
+        }
+        else
+        {
+            //echo('stop details did not return result');
+            return false;
+        }
+    }
+
+
+
     public function stop_details($stop_id)
     {
         $this->db->where('stop_id',$stop_id);
