62614da22987370b35edcae427f4cff1ae3e13f2
urandu <bildadnamawa@gmail.com>
2015-05-24 00:00:01 +0300

    urandu made changes at Sun May 24 00:00:01 EAT 2015 on trans_mobility

--- application/helpers/my_helper.php
+++ application/helpers/my_helper.php
@@ -65,3 +65,29 @@ function get_route_town_terminus($route_id)
     $CI->load->model('route_model');
     return $CI->route_model->route_town_terminus($route_id);
 }
+
+
+
+
+
+
+
+/**
+based on haversine algorithm
+ */
+function distance($lat1, $lon1, $lat2, $lon2) {
+
+    $earth_radius = 3960.00; # in miles
+    $delta_lat = $lat2 - $lat1 ;
+    $delta_lon = $lon2 - $lon1 ;
+
+    $alpha    = $delta_lat/2;
+    $beta     = $delta_lon/2;
+    $a        = sin(deg2rad($alpha)) * sin(deg2rad($alpha)) + cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * sin(deg2rad($beta)) * sin(deg2rad($beta)) ;
+    $c        = asin(min(1, sqrt($a)));
+    $distance = 2*$earth_radius * $c;
+    $distance = round($distance, 4);
+
+    return $distance;
+}
+
